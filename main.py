deb85d779b4fd0ec826e26bb4cd128f9ac3c8205
HyTacker20 <hytacker@gmail.com>
2020-12-14 18:21:03 +0200

    Fix #5. Created a separate module

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-import json
+# import json
+import gamer
 
-with open('games.json', 'r', encoding='utf-8') as f:
-    games = json.load(f)
+# with open('games.json', 'r', encoding='utf-8') as f:
+    # games = json.load(f)
     # print(games)
 
-for game in games['gameList']:
-    print('Назва гри - ', game['gameName'], '\n')
-    print('Кількість гравців - ', game['playersNumber'], '\n')
-    print('Тип гри - ', game['gameType'], '\n')
-    print('Наявність інвентаря - ', game['inventory'], '\n')
-    print('Місце для гри - ', game['location'], '\n')
+# for game in games['gameList']:
+    # print('Назва гри - ', game['gameName'])
+    # print('Кількість гравців - ', game['playersNumber'])
+    # print('Тип гри - ', game['gameType'])
+    # print('Наявність інвентаря - ', game['inventory'])
+    # print('Місце для гри - ', game['location'], '\n')
+
+
+# print('-------------------------------------------------------------')
+
+
+gamer.get_games()
