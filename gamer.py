d9b484aeecf6eb66f5e7a99e2f7719bbd7ce335c
HyTacker20 <hytacker@gmail.com>
2020-12-21 13:17:10 +0200

    Fix #5. Fixed the separate module

--- gamer.py
+++ gamer.py
@@ -1,13 +1,9 @@
 import json
 
 def get_games():
+
+    games = None
     with open('games.json', 'r', encoding='utf-8') as f:
         games = json.load(f)
-        # print(games)
 
-    for game in games['gameList']:
-        print('Назва гри - ', game['gameName'])
-        print('Кількість гравців - ', game['playersNumber'])
-        print('Тип гри - ', game['gameType'])
-        print('Наявність інвентаря - ', game['inventory'])
-        print('Місце для гри - ', game['location'], '\n')
+    return games
--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-# import json
 import gamer
 
-# with open('games.json', 'r', encoding='utf-8') as f:
-    # games = json.load(f)
-    # print(games)
 
-# for game in games['gameList']:
-    # print('Назва гри - ', game['gameName'])
-    # print('Кількість гравців - ', game['playersNumber'])
-    # print('Тип гри - ', game['gameType'])
-    # print('Наявність інвентаря - ', game['inventory'])
-    # print('Місце для гри - ', game['location'], '\n')
+games = gamer.get_games()
 
-
-# print('-------------------------------------------------------------')
-
-
-gamer.get_games()
+for game in games['gameList']:
+    print('Назва гри - ', game['gameName'])
+    print('Кількість гравців - ', game['playersNumber'])
+    print('Тип гри - ', game['gameType'])
+    print('Наявність інвентаря - ', game['inventory'])
+    print('Місце для гри - ', game['location'], '\n')
