805976575e166fb84999465d6c6f7c8db2b1e1dc
HyTacker20 <hytacker@gmail.com>
2021-01-09 15:04:58 +0200

    Fix #8. Finished filtering games

--- filtering.py
+++ filtering.py
@@ -1,9 +1,40 @@
 def filter_by_players_count(gamesList, needed_player):
-
-    good_games = []
+    good_games1 = []
 
     for game in gamesList :
         if needed_player >= game['minPlayersNumber']:
-            good_games.append(game)
+            good_games1.append(game)
 
-    return good_games
+    return good_games1
+
+
+def filter_by_inventory(gamesList, needed_inventory):
+    good_games2 = []
+
+    for game in gamesList :
+        if needed_inventory == game['inventory']:
+            good_games2.append(game)
+
+    return good_games2
+
+
+def filter_by_type(gamesList, needed_type):
+    good_games3 = []
+
+    for game in gamesList :
+        if needed_type == game['gameType']:
+            good_games3.append(game)
+
+    return good_games3
+
+
+def filter_by_location(gamesList, needed_location):
+    good_games4 = []
+
+    for game in gamesList :
+        if needed_location == game['location'] or game['location'] == 'байдуже':
+            good_games4.append(game)
+        # elif game['gameLocation'] == 'байдуже':
+
+
+    return good_games4
--- main.py
+++ main.py
@@ -2,18 +2,49 @@ import gamer
 import filtering
 
 games = gamer.get_games()
-# for game in games['gameList']:
-#     print('Назва гри - ', game['gameName'])
-#     print('Кількість гравців - ', game['minPlayersNumber'])
-#     print('Тип гри - ', game['gameType'])
-#     print('Наявність інвентаря - ', game['inventory'])
-#     print('Місце для гри - ', game['location'], '\n')
+
 
 needed_player = str(input('Скільки гравців? - '))
 print('\n', '-----------------------------------')
 
-filtered_list = filtering.filter_by_players_count(games['gameList'], needed_player)
-for game in filtered_list:
+filtered_list1 = filtering.filter_by_players_count(games['gameList'], needed_player)
+for game in filtered_list1:
+    print('Назва гри - ', game['gameName'])
+    print('Кількість гравців - ', game['minPlayersNumber'])
+    print('Тип гри - ', game['gameType'])
+    print('Наявність інвентаря - ', game['inventory'])
+    print('Місце для гри - ', game['location'], '\n')
+
+
+needed_inventory = str(input('Наявність інвентаря?(канцелярія | спортивний | підручні | нема) - ').lower())
+print('\n', '-----------------------------------')
+
+filtered_list2 = filtering.filter_by_inventory(filtered_list1, needed_inventory)
+for game in filtered_list2:
+    print('Назва гри - ', game['gameName'])
+    print('Кількість гравців - ', game['minPlayersNumber'])
+    print('Тип гри - ', game['gameType'])
+    print('Наявність інвентаря - ', game['inventory'])
+    print('Місце для гри - ', game['location'], '\n')
+
+
+needed_type = str(input('Який тип гри?(активна | пасивна) - ').lower())
+print('\n', '-----------------------------------')
+
+filtered_list3 = filtering.filter_by_type(filtered_list2, needed_type)
+for game in filtered_list3:
+    print('Назва гри - ', game['gameName'])
+    print('Кількість гравців - ', game['minPlayersNumber'])
+    print('Тип гри - ', game['gameType'])
+    print('Наявність інвентаря - ', game['inventory'])
+    print('Місце для гри - ', game['location'], '\n')
+
+
+needed_location = str(input('Де будете грати?(всередині | зовні) - ').lower())
+print('\n', '-----------------------------------')
+
+filtered_list4 = filtering.filter_by_location(filtered_list3, needed_location)
+for game in filtered_list4:
     print('Назва гри - ', game['gameName'])
     print('Кількість гравців - ', game['minPlayersNumber'])
     print('Тип гри - ', game['gameType'])
