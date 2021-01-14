661c35d5a922fbdfda79e18aa2fa77ef3a790520
HyTacker20 <hytacker@gmail.com>
2021-01-14 12:20:15 +0200

    Fix #9. Rename some variables.

--- filtering.py
+++ filtering.py
@@ -28,7 +28,7 @@ def filter_by_location(gamesList, needed_location):
     return filtered_games
 
 def final_filter(gamesList, needed_data):
-    filtered_games1 = filter_by_players_count(gamesList, needed_data[0])
-    filtered_games2 = filter_by_inventory(filtered_games1, needed_data[1])
-    filtered_games3 = filter_by_location(filtered_games2, needed_data[2])
-    return filtered_games3
+    filtered_games = filter_by_players_count(gamesList, needed_data[0])
+    filtered_games = filter_by_inventory(filtered_games1, needed_data[1])
+    filtered_games = filter_by_location(filtered_games2, needed_data[2])
+    return filtered_games
