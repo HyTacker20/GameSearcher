97a00f38dd8d799c818f15980b1776ff4306fd45
alex-dubchak <alex.dubchak@gmail.com>
2021-01-15 13:02:32 +0200

    Fix #7. Created module for filtering  (#10)
    * Fix #7. Created module for filtering by players number.
    
    * Fix #8. Finished filtering games
    
    * Fix #9. Combined filtering into one function.
    
    * Fix #9. Delete pycache
    
    * Fix #9.
    
    * Fix #9. Rename some variables.
    
    * Fix #9. Fixed a bug in the code
    
    Co-authored-by: HyTacker20 <hytacker@gmail.com>

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import gamer
-
+import filtering
 
 games = gamer.get_games()
 
-for game in games['gameList']:
+needed_data=[]
+
+needed_data.append(int(input('Скільки гравців? - ')))
+print('\n', '-----------------------------------')
+needed_data.append(str(input('Наявність інвентаря?(канцелярія | спортивний | підручні | нема) - ').lower()))
+print('\n', '-----------------------------------')
+needed_data.append(str(input('Де будете грати?(всередині | зовні) - ').lower()))
+print('\n', '-----------------------------------')
+
+final_list = filtering.final_filter(games['gameList'], needed_data)
+
+for game in final_list:
     print('Назва гри - ', game['gameName'])
-    print('Кількість гравців - ', game['playersNumber'])
-    print('Тип гри - ', game['gameType'])
+    print('мін. Кількість гравців - ', game['minPlayersNumber'])
     print('Наявність інвентаря - ', game['inventory'])
     print('Місце для гри - ', game['location'], '\n')
