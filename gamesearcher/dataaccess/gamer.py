12601248348e800f095723848c8f0073ad522eb6
alex-dubchak <alex.dubchak@gmail.com>
2021-02-18 10:07:29 +0200

    Update gamer.py

--- gamesearcher/dataaccess/gamer.py
+++ gamesearcher/dataaccess/gamer.py
@@ -6,7 +6,7 @@ logging.basicConfig(level = logging.INFO)
 def get_games():
 
     games = None
-    with open('../data/games.json', 'r', encoding='utf-8') as f:
+    with open('data/games.json', 'r', encoding='utf-8') as f:
         games = json.load(f)
 
     logger.info(' %s games loaded', len(games['gameList']))
