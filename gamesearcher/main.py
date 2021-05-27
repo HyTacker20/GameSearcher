6099ff10fc1b2eb9c131e5f2c8e6436b4eaaff31
HyTacker20 <hermak20102004@gmail.com>
2021-05-27 16:09:48 +0300

    Fix the best ficha! 2.0

--- gamesearcher/main.py
+++ gamesearcher/main.py
@@ -141,6 +141,7 @@ def send_data(msg):
 @bot.message_handler(commands=['for_alex'])
 def help_alex(msg):
     with open('data/help-alexey.mp3', 'r') as audio:
-        bot.send_audio(msg.chat.id, audio, 'Ой, Льоха, боту без тебе плохо!', reply_markup=kb.home())
+        bot.send_audio(msg.chat.id, audio, 'Ой, Льоха, боту без тебе плохо!')
+    bot.send_message(msg.chat.id, msg1, parse_mode='markdown', reply_markup=kb.home())
 
 bot.polling(none_stop=True, interval=0)
